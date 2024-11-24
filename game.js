bf60f455ecd38fba1367ff3e8060b66a6ac5f3ec
Lidia2509 <pali24ee@student.ju.se>
2024-11-24 12:54:15 +0100

    added grass and rock on background

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ let stars = [];
 const regionX = 350;
 const regionY = 200;
 const regionWidth = 150;
-const regionHeight = 200;
+const regionHeight = 200; 
 
 //function for the canvas and the stars to have them in the rect
 function setup() {
@@ -119,13 +119,22 @@ function gameScreen() {
     tree(30, 450);
     tree(80, 640);
   
-    bush(50, 200);
-    bush(90, 550);
+    bush(50, 200, 0.5);
+  bush(90, 550, 0.5);
   
     //sand
     fill(180, 190, 50);
     rect(450, 0, 100, height);
 
+    rock(470, 100);
+  rock(490, 200);
+  rock(475, 400);
+  rock(500, 550);
+
+  bush(480, 50, 0.2);
+  bush(490, 250, 0.2);
+  bush(470, 450, 0.2);
+  bush(475, 580, 0.2);
   
     //lake
     fill(20, 140, 160, 100);
@@ -138,6 +147,14 @@ function gameScreen() {
 
 let treeX = 80; 
 let treeY = 300;
+let treeS = 1;
+let rockX = 490;
+let rockY = 40;
+
+function rock(rockX, rockY) {
+  fill(95, 90, 90);
+  ellipse(rockX, rockY, 10, 15);
+}
 
 function tree(treeX, treeY) {
   //tree
@@ -164,32 +181,25 @@ function tree(treeX, treeY) {
   ellipse(treeX + 25, treeY - 25, 30);
 }
 
-function bush(treeX, treeY) {
-  push();
-
-  translate(treeX, treeY);
-  scale(0.6);
-  translate(-treeX, -treeY);
-
+function bush(treeX, treeY, treeS) {
   fill(55, 170, 15);
-  ellipse(treeX, treeY, 80);
+  ellipse(treeX, treeY, 80 * treeS);
 
   //left side leaves
-  ellipse(treeX - 40, treeY - 5, 30);
-  ellipse(treeX - 35, treeY + 20, 30);
-  ellipse(treeX - 20, treeY + 30, 25);
-  ellipse(treeX - 25, treeY - 25, 30);
+  ellipse(treeX - 40 * treeS, treeY - 5 * treeS, 30 * treeS);
+  ellipse(treeX - 35 * treeS, treeY + 20 * treeS, 30 * treeS);
+  ellipse(treeX - 20 * treeS, treeY + 30 * treeS, 25 * treeS);
+  ellipse(treeX - 25 * treeS, treeY - 25 * treeS, 30 * treeS);
 
   //middle leaves
-  ellipse(treeX, treeY - 35, 30, 25);
-  ellipse(treeX, treeY + 32, 25);
+  ellipse(treeX, treeY - 35 * treeS, 30 * treeS, 25 * treeS);
+  ellipse(treeX, treeY + 32 * treeS, 25 * treeS);
 
   //right side leaves
-  ellipse(treeX + 40, treeY - 5, 30);
-  ellipse(treeX + 35, treeY + 20, 30);
-  ellipse(treeX + 20, treeY + 30, 25);
-  ellipse(treeX + 25, treeY - 25, 30);
-  pop();
+  ellipse(treeX + 40 * treeS, treeY - 5 * treeS, 30 * treeS);
+  ellipse(treeX + 35 * treeS, treeY + 20 * treeS, 30 * treeS);
+  ellipse(treeX + 20 * treeS, treeY + 30 * treeS, 25 * treeS);
+  ellipse(treeX + 25 * treeS, treeY - 25 * treeS, 30 * treeS);
 }
 
 function player(x, y, s) {
@@ -915,7 +925,7 @@ function draw() {
 }
 
 function mouseClicked() {
-  if (
+  if ( 
     state === "start" &&
     mouseX >= 180 &&
     mouseX <= 319 &&
@@ -925,7 +935,7 @@ function mouseClicked() {
     state = "game";
   } else if (
     (state === "resultSuccess" &&
-      mouseX >= 200 &&
+      mouseX >= 200 && 
       mouseX <= 400 &&
       mouseY >= 280 &&
       mouseY <= 330) ||
